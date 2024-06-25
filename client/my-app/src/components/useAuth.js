899c81c49af842648dbe421dee2ecc22577b1211
michaelvong <mvong006@ucr.edu>
2024-06-25 00:01:16 -0700

    added a hook to refresh token near expiration

--- client/my-app/src/components/useAuth.js
+++ client/my-app/src/components/useAuth.js
@@ -7,6 +7,7 @@ export default function UseAuth(code){
     const [refreshToken, setRefreshToken] = useState();
     
     useEffect(() => {
+        
         axios.post('http://localhost:3001/auth', {
             client_id : process.env.REACT_APP_TWITCH_CLIENT_ID,
             redirect_uri : "http://localhost:3000",
@@ -14,16 +15,39 @@ export default function UseAuth(code){
             code : code,
             grant_type : "authorization_code"
         }).then((response) => {
-            //console.log(response);
-            setAccessToken(response.access_token);
-            setExpiresIn(response.expires_in);
-            setRefreshToken(response.refresh_token);
-            console.log(response);
+            //console.log(response.data);
+            setAccessToken(response.data.access_token);
+            setExpiresIn(response.data.expires_in);
+            setRefreshToken(response.data.refresh_token);
             window.history.pushState({}, null, "/")
         }).catch(() => {
             //window.location = "/";
         })
     }, [code])
+
+    useEffect(() => {
+        //if statement to do nothing if no refresh token or expires in
+        if(!refreshToken || !expiresIn) {
+            return
+        }
+        const interval = setInterval(() => {
+            axios.post("http://localhost:3001/refresh", {
+                refreshToken,
+                client_id : process.env.REACT_APP_TWITCH_CLIENT_ID,
+                client_secret : process.env.REACT_APP_TWITCH_CLIENT_SECRET,
+            }).then(res => {
+                //console.log(res.data)
+                setAccessToken(res.data.access_token)
+                //setRefreshToken(res.data.refresh_token)
+                setExpiresIn(res.data.expires_in)
+                //window.history.pushState({}, null, "/")
+            }).catch(() => {
+                window.location = "/"
+            })
+        }, (expiresIn - 60) * 1000) 
+        //subtract 60 seconds and times 1000 to convert seconds to milliseconds 
+        return () => clearInterval(interval)  
+    }, [refreshToken, expiresIn])
 
     return accessToken;
 };
