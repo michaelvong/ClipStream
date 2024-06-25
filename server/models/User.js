dfa98e55af37cce16c998c37ff525d0ef17a6778
michaelvong <mvong006@ucr.edu>
2024-06-24 23:59:01 -0700

    added my JWT refresh token to model

--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     twitch_access_token : String,
     twitch_refresh_token : String,
     access_token: String,
+    refresh_token: String,
 });
 
 module.exports = model('User', UserSchema);
