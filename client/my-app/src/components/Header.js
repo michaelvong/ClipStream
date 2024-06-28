96c6e2ef0d92950997e78f8e464519e3595fe948
michaelvong <mvong006@ucr.edu>
2024-06-27 21:42:18 -0700

    created a header for web page

---
+++ client/my-app/src/components/Header.js
@@ -0,0 +1,32 @@
+export default function Header() {
+
+    return (
+        <div class="bg-gray-100">
+            <header class="w-full text-gray-700 bg-gray-100 border-t border-gray-100 shadow-sm body-font">
+                <div class="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+
+                    <nav class="flex flex-wrap items-center text-base lg:w-2/5 md:ml-auto">
+                        <a href="#_" class="mr-5 font-medium hover:text-gray-900">Home</a>
+                        <a href="#_" class="mr-5 font-medium hover:text-gray-900">About</a>
+                        <a href="#_" class="font-medium hover:text-gray-900">Contact</a>
+                    </nav>
+                    <a
+                        class="flex items-center order-first mb-4 font-medium text-gray-900 lg:order-none lg:w-1/5 title-font lg:items-center lg:justify-center md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m7.848 8.25 1.536.887M7.848 8.25a3 3 0 1 1-5.196-3 3 3 0 0 1 5.196 3Zm1.536.887a2.165 2.165 0 0 1 1.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 1 1-5.196 3 3 3 0 0 1 5.196-3Zm1.536-.887a2.165 2.165 0 0 0 1.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863 2.077-1.199m0-3.328a4.323 4.323 0 0 1 2.068-1.379l5.325-1.628a4.5 4.5 0 0 1 2.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.33 4.33 0 0 0 10.607 12m3.736 0 7.794 4.5-.802.215a4.5 4.5 0 0 1-2.48-.043l-5.326-1.629a4.324 4.324 0 0 1-2.068-1.379M14.343 12l-2.882 1.664" />
+                        </svg>
+                        <div class="text-lg">
+                            ClipStream
+                        </div>
+                    </a>
+                    <div class="inline-flex items-center h-full ml-5 lg:w-2/5 lg:justify-end lg:ml-0">
+                        <a href="#_"
+                            class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-teal-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
+                            Login
+                        </a>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
