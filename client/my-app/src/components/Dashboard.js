8b566702d863f051b5476105842f0ce16d63bdd6
michaelvong <mvong006@ucr.edu>
2024-07-01 22:07:47 -0700

    refactored

--- client/my-app/src/components/Dashboard.js
+++ client/my-app/src/components/Dashboard.js
@@ -7,30 +7,13 @@ import ClipsGrid from "./ClipsGrid";
 export default function Dashboard({code}){
     const [clips, setClips] = useState();
     let access_token = localStorage.getItem("access_token")
-    if(access_token === 'undefined'){
+    console.log(access_token);
+    if(access_token === 'undefined'  || !access_token){
         access_token = UseAuth(code);
         localStorage.setItem("access_token", access_token);
         console.log(access_token);
     }
     
-    //const temp = localStorage.getItem("access_token");
-    async function test(){
-        const config = {
-            method: 'get',
-            url : 'http://localhost:3001/clips',
-            headers : {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-                'Client-Id': process.env.REACT_APP_TWITCH_CLIENT_ID,
-            },
-        }
-        axios(config) 
-        .then((response) => {
-            console.log(response);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }  
 
     useEffect(() => {
         const config = {
@@ -62,19 +45,19 @@ export default function Dashboard({code}){
     return (
         <div>
             <Header/>
-            <div>
-                Followed clips
-            </div>
-            <iframe src="https://clips.twitch.tv/embed?clip=InnocentAntediluvianCarabeefPanicBasket-6MfF_-tWB7yv0_CD&parent=localhost" sandbox="allow-same-origin allow-scripts" frameborder="0" allowfullscreen="true" height="378" width="620">ts</iframe>
-
-            <button onClick={test}>test</button>
-            {clips?.length > 0 && 
-            <div>
-                <ClipsGrid clips={clips}>
-                    
-                </ClipsGrid>
-            </div>}
+            <div className="p-10 bg-gray-200">
+                <div>
+                    Followed clips
+                </div>                     
+                {clips?.length > 0 && 
+                <div>
+                    <ClipsGrid clips={clips}>
+                        
+                    </ClipsGrid>
+                </div>}
+            </div> 
         </div>
+        
         
                 
     );
